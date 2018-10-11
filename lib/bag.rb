76f80e1327f1e4913e385dbccf4198c5f8db4c18
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-10-11 14:11:22 +0100

    Create populate_bag method

--- lib/bag.rb
+++ lib/bag.rb
@@ -1,5 +1,14 @@
 class Bag
-  def contents
-    []
+  attr_reader :contents
+
+  def initialize(colour:, counter_class:)
+    @contents = []
+    populate_bag(colour, counter_class)
+  end
+
+  private
+
+  def populate_bag(colour, counter_class)
+    3.times { @contents << counter_class.new(colour)}
   end
 end
--- spec/bag_spec.rb
+++ spec/bag_spec.rb
@@ -2,8 +2,12 @@ require 'bag'
 
 describe Bag, :bag do
   describe '#contents', :contents do
-    it 'Is empty' do
-      expect(subject.contents).to be_empty
+    let(:subject) { described_class.new(colour: "white", counter_class: counter_class)}
+    let(:counter_class) { double(:counter_class, new: white_counter) }
+    let(:white_counter) { double(:white_counter)}
+
+    it 'contains 3 white counters' do
+      expect(subject.contents).to eq([white_counter, white_counter, white_counter])
     end
 
     it 'Is an array' do
